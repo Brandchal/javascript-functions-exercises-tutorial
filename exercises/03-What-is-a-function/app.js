0f2cd2cf88848977fda9c28282e691a46552afa1
Brandchal <Bchavarrial@hotmail.com>
2021-02-12 03:12:50 +0000

    update

--- exercises/03-What-is-a-function/app.js
+++ exercises/03-What-is-a-function/app.js
@@ -1,12 +1,14 @@
-function sum(number1,number2) {
-    return number1 + number2;
+function sum(number1,number2) { //Este es el constructor de la funsión.
+    return number1 + number2;  //Le indicamos que hacer, de acuerdo al constructor
 }
 
 var total = sum(2,3);
 var total2 = sum(5,10);
 var total3 = sum(2,3);
+var superduper = sum(3445324,53454423)
 
 
 console.log(total);
 console.log(total2);
 console.log(total3);
+console.log(superduper);
