869366e0e6a49e47b765d1b52eec6a660b995dfc
Brandchal <Bchavarrial@hotmail.com>
2021-02-13 02:44:02 +0000

    Finish

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,8 +1,16 @@
-() =>{
-    
+const rapid = (x)=>{
+x = x.toLowerCase()
+let newStr = ''
+for (i=0; i<x.length;i++){
+if (x[i] !== 'a' && x[i] !== 'e' && x[i] !== 'i' && x[i] !== 'o' && x[i] !== 'u'){
+newStr += x[i].toUpperCase()
 }
-   
-   // From this line up Do not change code below
-  let str = "John";
-  console.log(rapid(str));
+}
+return newStr
+}
+
+// From this line up Do not change code below
+let str = "John";
+
+console.log(rapid(str));
 
